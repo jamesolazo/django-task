97430b7d59c62fc774ccd5cdff35b7e8b34623f5
Richard Olazo <rolazo.web@gmail.com>
2020-04-18 10:20:56 +0800

    db set up

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bk96q2fv4t4$$w)bat2x+0ba9z-*(w97g3$6yr$&f0horfqye='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'agile.apps.AgileConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_task',
+        'USER': 'root',
+        'PASSWORD': 'richardpass',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
