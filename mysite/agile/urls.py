fc0d75ced794d9572625ee4d5f0d555bf502e3b6
Richard Olazo <rolazo.web@gmail.com>
2020-04-22 10:00:52 +0800

    using pipenv

--- mysite/agile/urls.py
+++ mysite/agile/urls.py
@@ -1,8 +1,8 @@
-from .views import PrincipleListView, ValuesListView
+from .views import PrincipleList, ValuesList
 
 from django.urls import path
 
 urlpatterns = [
-    path('principles', PrincipleListView.as_view(), name='principle-list'),
-    path('values', ValuesListView.as_view(), name='values-list'),
+    path('principles', PrincipleList.as_view(), name='principle-list'),
+    path('values', ValuesList.as_view(), name='values-list'),
 ]
--- mysite/agile/views.py
+++ mysite/agile/views.py
@@ -1,27 +1,14 @@
 from .models import Values, Principles
 from .serializer import ValuesSerializer, PrincipleSerializer
 
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_500_INTERNAL_SERVER_ERROR
-from rest_framework.views import APIView
+from rest_framework import generics
 
 
-class ValuesListView(APIView):
-    def get(self, request):
-        try:
-            values = Values.objects.all()
-            serializer = ValuesSerializer(values, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except Exception:
-            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+class ValuesList(generics.ListCreateAPIView):
+    queryset = Values.objects.all()
+    serializer_class = ValuesSerializer
 
 
-class PrincipleListView(APIView):
-    def get(self, request):
-        try:
-            principles = Principles.objects.all()
-            serializer = PrincipleSerializer(principles, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except Exception:
-            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+class PrincipleList(generics.ListCreateAPIView):
+    queryset = Principles.objects.all()
+    serializer_class = PrincipleSerializer
