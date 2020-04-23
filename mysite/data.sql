75ae0d1e8f1fcc85a12c2e381a85131a319340fa
Richard Olazo <rolazo.web@gmail.com>
2020-04-23 08:15:24 +0800

    used sqlite

--- mysite/data.sql
+++ mysite/data.sql
@@ -1,54 +1,19 @@
--- VALUES DATA --
+-- PRINCIPLES --
+INSERT INTO agile_principles (id, description, created_at, updated_at) VALUES(1, 'Our highest priority is to satisfy the customer through early and continuous delivery of valuable software.', '2020-04-18 10:14:49', '2020-04-18 10:14:49');
+INSERT INTO agile_principles (id, description, created_at, updated_at) VALUES(2, 'Welcome changing requirements, even late in development. Agile processes harness change for the customer’s competitive advantage.', '2020-04-18 10:14:58', '2020-04-18 10:15:00');
+INSERT INTO agile_principles (id, description, created_at, updated_at) VALUES(3, 'Deliver working software frequently, from a couple of weeks to a couple of months, with a preference to the shorter timescale.', '2020-04-18 10:15:17', '2020-04-18 10:15:18');
+INSERT INTO agile_principles (id, description, created_at, updated_at) VALUES(4, 'Business people and developers must work together daily throughout the project.', '2020-04-18 10:15:39', '2020-04-18 10:15:40');
+INSERT INTO agile_principles (id, description, created_at, updated_at) VALUES(5, 'Build projects around motivated individuals. Give them the environment and support they need, and trust them to get the job done.', '2020-04-18 10:16:19', '2020-04-18 10:16:20');
+INSERT INTO agile_principles (id, description, created_at, updated_at) VALUES(6, 'The most efficient and effective method of conveying information to and within a development team is face-to-face conversation.', '2020-04-18 10:16:55', '2020-04-18 10:16:56');
+INSERT INTO agile_principles (id, description, created_at, updated_at) VALUES(7, 'Working software is the primary measure of progress.', '2020-04-18 10:17:20', '2020-04-18 10:17:21');
+INSERT INTO agile_principles (id, description, created_at, updated_at) VALUES(8, 'Agile processes promote sustainable development. The sponsors, developers, and users should be able to maintain a constant pace indefinitely.', '2020-04-18 10:17:32', '2020-04-18 10:17:32');
+INSERT INTO agile_principles (id, description, created_at, updated_at) VALUES(9, 'Continuous attention to technical excellence and good design enhances agility.', '2020-04-18 10:17:42', '2020-04-18 10:17:43');
+INSERT INTO agile_principles (id, description, created_at, updated_at) VALUES(10, 'Simplicity — the art of maximizing the amount of work not done — is essential.', '2020-04-18 10:17:51', '2020-04-18 10:17:53');
+INSERT INTO agile_principles (id, description, created_at, updated_at) VALUES(11, 'The best architectures, requirements, and designs emerge from self-organizing teams.', '2020-04-18 10:18:07', '2020-04-18 10:18:08');
+INSERT INTO agile_principles (id, description, created_at, updated_at) VALUES(12, 'At regular intervals, the team reflects on how to become more effective, then tunes and adjusts its behavior accordingly.', '2020-04-18 10:18:15', '2020-04-18 10:18:16');
 
-INSERT INTO django_task.agile_values
-(id, title, description, created_at, updated_at)
-VALUES(1, 'Individuals and interactions over processes and tools', '<p>The value #1 in the Agile Manifesto is known as “Individuals and interactions over processes and tools.” It’s crucial to value people more highly than processes or tools. It is the people who respond to business needs and drive the development process. When the development is driven by the process or tools, then the team is less responsive to changes and less likely to meet customer needs</p>', '2020-04-18 10:10:53', '2020-04-18 10:10:54');
-INSERT INTO django_task.agile_values
-(id, title, description, created_at, updated_at)
-VALUES(2, 'Software over documentation', '<p>Historically, documenting the product for development and ultimate delivery required enormous amounts of time. Technical requirements, specifications, prospectus, testing plans, interface design documents, and different approvals were required for each. There were long delays in development.</p>', '2020-04-18 10:11:26', '2020-04-18 10:11:27');
-INSERT INTO django_task.agile_values
-(id, title, description, created_at, updated_at)
-VALUES(3, 'Customer collaboration over contract negotiation', '<p>Negotiations are important. It’s about the period when customers and a product manager work out the details of a delivery with all the details. However, collaboration is a different creature entirely and it really matters.</p>', '2020-04-18 10:13:56', '2020-04-18 10:13:57');
-INSERT INTO django_task.agile_values
-(id, title, description, created_at, updated_at)
-VALUES(4, 'Responding to changes over following a plan', '<p>Traditional software development regarded change as an expense. It was to be avoided.</p>', '2020-04-18 10:14:30', '2020-04-18 10:14:30');
-
-
--- PRINCIPLES DATA --
-
-INSERT INTO django_task.agile_principles
-(id, description, created_at, updated_at)
-VALUES(1, 'Our highest priority is to satisfy the customer through early and continuous delivery of valuable software.', '2020-04-18 10:14:49', '2020-04-18 10:14:49');
-INSERT INTO django_task.agile_principles
-(id, description, created_at, updated_at)
-VALUES(2, 'Welcome changing requirements, even late in development. Agile processes harness change for the customer’s competitive advantage.', '2020-04-18 10:14:58', '2020-04-18 10:15:00');
-INSERT INTO django_task.agile_principles
-(id, description, created_at, updated_at)
-VALUES(3, 'Deliver working software frequently, from a couple of weeks to a couple of months, with a preference to the shorter timescale.', '2020-04-18 10:15:17', '2020-04-18 10:15:18');
-INSERT INTO django_task.agile_principles
-(id, description, created_at, updated_at)
-VALUES(4, 'Business people and developers must work together daily throughout the project.', '2020-04-18 10:15:39', '2020-04-18 10:15:40');
-INSERT INTO django_task.agile_principles
-(id, description, created_at, updated_at)
-VALUES(5, 'Build projects around motivated individuals. Give them the environment and support they need, and trust them to get the job done.', '2020-04-18 10:16:19', '2020-04-18 10:16:20');
-INSERT INTO django_task.agile_principles
-(id, description, created_at, updated_at)
-VALUES(6, 'The most efficient and effective method of conveying information to and within a development team is face-to-face conversation.', '2020-04-18 10:16:55', '2020-04-18 10:16:56');
-INSERT INTO django_task.agile_principles
-(id, description, created_at, updated_at)
-VALUES(7, 'Working software is the primary measure of progress.', '2020-04-18 10:17:20', '2020-04-18 10:17:21');
-INSERT INTO django_task.agile_principles
-(id, description, created_at, updated_at)
-VALUES(8, 'Agile processes promote sustainable development. The sponsors, developers, and users should be able to maintain a constant pace indefinitely.', '2020-04-18 10:17:32', '2020-04-18 10:17:32');
-INSERT INTO django_task.agile_principles
-(id, description, created_at, updated_at)
-VALUES(9, 'Continuous attention to technical excellence and good design enhances agility.', '2020-04-18 10:17:42', '2020-04-18 10:17:43');
-INSERT INTO django_task.agile_principles
-(id, description, created_at, updated_at)
-VALUES(10, 'Simplicity — the art of maximizing the amount of work not done — is essential.', '2020-04-18 10:17:51', '2020-04-18 10:17:53');
-INSERT INTO django_task.agile_principles
-(id, description, created_at, updated_at)
-VALUES(11, 'The best architectures, requirements, and designs emerge from self-organizing teams.', '2020-04-18 10:18:07', '2020-04-18 10:18:08');
-INSERT INTO django_task.agile_principles
-(id, description, created_at, updated_at)
-VALUES(12, 'At regular intervals, the team reflects on how to become more effective, then tunes and adjusts its behavior accordingly.', '2020-04-18 10:18:15', '2020-04-18 10:18:16');
+-- VALUES --
+INSERT INTO agile_values (id, title, description, created_at, updated_at) VALUES(1, 'Individuals and interactions over processes and tools', '<p>The value #1 in the Agile Manifesto is known as “Individuals and interactions over processes and tools.” It’s crucial to value people more highly than processes or tools. It is the people who respond to business needs and drive the development process. When the development is driven by the process or tools, then the team is less responsive to changes and less likely to meet customer needs</p>', '2020-04-18 10:10:53', '2020-04-18 10:10:54');
+INSERT INTO agile_values (id, title, description, created_at, updated_at) VALUES(2, 'Software over documentation', '<p>Historically, documenting the product for development and ultimate delivery required enormous amounts of time. Technical requirements, specifications, prospectus, testing plans, interface design documents, and different approvals were required for each. There were long delays in development.</p>', '2020-04-18 10:11:26', '2020-04-18 10:11:27');
+INSERT INTO agile_values (id, title, description, created_at, updated_at) VALUES(3, 'Customer collaboration over contract negotiation', '<p>Negotiations are important. It’s about the period when customers and a product manager work out the details of a delivery with all the details. However, collaboration is a different creature entirely and it really matters.</p>', '2020-04-18 10:13:56', '2020-04-18 10:13:57');
+INSERT INTO agile_values (id, title, description, created_at, updated_at) VALUES(4, 'Responding to changes over following a plan', '<p>Traditional software development regarded change as an expense. It was to be avoided.</p>', '2020-04-18 10:14:30', '2020-04-18 10:14:30');
